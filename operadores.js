041b4f84b5433cafa9c3fd49d6410873712bf3cb
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-16 16:08:07 -0500

    operador logico ||

--- operadores.js
+++ operadores.js
@@ -1,8 +1,17 @@
+
+console.log("And")
 console.log("1",true && true)//VERDADERO
 console.log("2",false && true)//FALSO
 console.log("3",true && false)//FALSO
 console.log("4",false && false)//FALSO
 
+
+console.log("||")
+
+console.log("1",true && true)//VERDADERO
+console.log("2",false && true)//FALSO
+console.log("3",true && false)//FALSO
+console.log("4",false && false)//FALSO
 ////https://estradawebgroup.com/Post/Introduccion-a-los-operadores-logicos-de-JavaScript/20564
 
 

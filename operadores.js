a06b40926ceab4c4842896bb6dacfd27f752597e
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-16 12:27:15 -0500

    Ejercicio de operadores

---
+++ operadores.js
@@ -0,0 +1,13 @@
+console.log("1",true && true)
+console.log("2",false && true)
+console.log("3",true && false)
+console.log("4",false && false)
+
+
+
+
+let elegible = false,
+    requerido = true;
+
+console.log(!elegible);
+console.log(!requerido);

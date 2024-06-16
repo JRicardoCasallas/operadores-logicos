3ba12307fbc47ba5c824d591cf69258db5d90c3e
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-16 12:37:13 -0500

    pantallazo de ejercicio de los operadores logicos

--- operadores.js
+++ operadores.js
@@ -1,7 +1,7 @@
-console.log("1",true && true)
-console.log("2",false && true)
-console.log("3",true && false)
-console.log("4",false && false)
+console.log("1",true && true)//VERDADERO
+console.log("2",false && true)//FALSO
+console.log("3",true && false)//FALSO
+console.log("4",false && false)//FALSO
 
 
 
